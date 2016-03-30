335f32e4963ee8bacc51ca2b79eac488e70f92fa
Don <mogul345@gmail.com>
2016-03-29 22:06:28 -0700

    Make the example app more useful for testing purposes.

--- app/src/main/java/com/dptsolutions/youtubeplayercustomoverlayexample/MainActivity.java
+++ app/src/main/java/com/dptsolutions/youtubeplayercustomoverlayexample/MainActivity.java
@@ -4,15 +4,28 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup activityChoices;
+    private RadioButton platformActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityChoices = (RadioGroup) findViewById(R.id.activity_type);
+        platformActivityButton = (RadioButton) findViewById(R.id.checkPlatform);
+
+        activityChoices.check(R.id.checkAppCompat);
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            platformActivityButton.setEnabled(false);
+        }
     }
 
     public void launchPlayer(View view) {
@@ -24,7 +37,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void doLaunchPlayer(boolean useCustomTheme) {
-        Class activityClass = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+        Class activityClass = activityChoices.getCheckedRadioButtonId() == R.id.checkPlatform
                 ? YouTubePlayerActivity.class
                 : YouTubePlayerAppCompatActivity.class;
         final Intent launchPlayerIntent = new Intent(this, activityClass);
